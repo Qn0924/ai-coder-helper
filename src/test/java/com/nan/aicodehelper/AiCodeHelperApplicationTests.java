16da338dbd7b3274b1fd8aae9a8772ed534b7f08
Qn0924 <2128904020@qq.com>
2025-08-17 17:21:58 +0800

    qwmodel的配置引用

--- src/test/java/com/nan/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/com/nan/aicodehelper/AiCodeHelperApplicationTests.java
@@ -1,13 +1,20 @@
 package com.nan.aicodehelper;
 
+import com.nan.aicodehelper.ai.AiCodeHelper;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AiCodeHelperApplicationTests {
 
+	@Resource
+    private AiCodeHelper aiCodeHelper;
     @Test
-    void contextLoads() {
+    void chat() {
+        aiCodeHelper.chat("你好，我是程序员秋楠");
     }
-
 }
---
+++ src/test/java/com/nan/aicodehelper/ai/AiCodeHelperServiceTest.java
@@ -0,0 +1,10 @@
+package com.nan.aicodehelper.ai;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class AiCodeHelperServiceTest {
+}
