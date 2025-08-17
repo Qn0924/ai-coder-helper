5c01c81794cf44c8f72edfc691280c146c26d80e
Qn0924 <2128904020@qq.com>
2025-08-17 14:08:10 +0800

    项目初始化

---
+++ src/main/java/com/nan/aicodehelper/AiCodeHelperApplication.java
@@ -0,0 +1,13 @@
+package com.nan.aicodehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeHelperApplication.class, args);
+    }
+
+}
