f8f7359d5a36de5131993ac1aba8d468a03cb171
Qn0924 <2128904020@qq.com>
2025-08-17 18:52:51 +0800

    多模态和系统预设

--- src/test/java/com/nan/aicodehelper/AiCodeHelperApplicationTests.java
+++ src/test/java/com/nan/aicodehelper/AiCodeHelperApplicationTests.java
@@ -17,4 +17,15 @@ class AiCodeHelperApplicationTests {
     void chat() {
         aiCodeHelper.chat("你好，我是程序员秋楠");
     }
+
+
+
+    @Test
+    void chatWithMessage() {
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("描述图片"),
+                ImageContent.from("https://ts1.tc.mm.bing.net/th/id/OIP-C.tHAIAaw4ZNlr4v2ldAkvYwHaHa?w=182&h=211&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2")
+        );
+        aiCodeHelper.chatWithMessage(userMessage);
+    }
 }
