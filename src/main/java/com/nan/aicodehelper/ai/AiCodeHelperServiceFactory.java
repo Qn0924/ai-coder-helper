253b2ab382a696d8409a0e0c0efba8c4fbc15d1e
Qn0924 <2128904020@qq.com>
2025-08-25 16:37:05 +0800

    工具调用Tool Calling

--- src/main/java/com/nan/aicodehelper/ai/AiCodeHelperServiceFactory.java
+++ src/main/java/com/nan/aicodehelper/ai/AiCodeHelperServiceFactory.java
@@ -1,5 +1,6 @@
 package com.nan.aicodehelper.ai;
 
+import com.nan.aicodehelper.ai.tools.InterviewQuestionTool;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
@@ -27,6 +28,7 @@ public class AiCodeHelperServiceFactory {
                 .chatModel(qwenChatModel)
                 .chatMemory(chatMemory)//会话记忆
                 .contentRetriever(contentRetriever)//RAG检索增强生成
+                .tools(new InterviewQuestionTool())//工具调用
                 .build();
         return aiCodeHelperService;
     }
